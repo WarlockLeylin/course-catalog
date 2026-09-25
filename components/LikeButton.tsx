b56e09be400ea37cdd24a4263c14fb6e63aa8127
Leylin <leylin.developer@gmail.com>
2026-09-25 12:42:59 +0500

    changed hover effect to orange in order to see it

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -12,7 +12,7 @@ export default function LikeButton({ initialLikes }: LikeButtonProps) {
   return (
     <button
       onClick={() => setLikes((prev) => prev + 1)}
-      className="px-4 py-2 border border-gray-300 rounded-md bg-gray-50 hover:bg-gray-100 transition font-medium text-gray-800"
+      className="px-4 py-2 border border-orange-300 rounded-md bg-gray-50 hover:bg-red-100 transition font-medium text-gray-800"
     >
       ❤ {likes}
     </button>
